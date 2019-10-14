4497f747a5f7e446124c89e4048654b2ad6e038f
Fang <1390803702@qq.com>
2019-10-15 01:14:08 +1400

    update

---
+++ linuxprogram/week6/code/exam/test3.c
@@ -0,0 +1,47 @@
+#include"lib.h"
+#include<dlfcn.h>
+
+int main()
+{
+	int a[MAX];
+	void *handle;
+	void (*f1)();
+	int (*f2)(),(*f3)();
+	char* error;
+	
+	handle = dlopen("/home/mouslet/Fang/linuxprogram/week6/code/dynamic/libmytestfang.so",RTLD_LAZY);
+	if(!handle)
+	{
+		fprintf(stderr,"%s\n",dlerror());
+		exit(1);
+	}
+	f1=dlsym(handle,"urand");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr,"%s\n",dlerror());
+		exit(1);
+	}
+	f2=dlsym(handle,"max");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr,"%s\n",dlerror());
+		exit(1);
+	}
+	f3=dlsym(handle,"sum");
+	if((error=dlerror())!=NULL)
+	{
+		fprintf(stderr,"%s\n",dlerror());
+		exit(1);
+	}
+	f1(a,MAX);
+	printf("max=%d \n*********\n",f2(a,MAX));
+	printf("sum=%d \n*********\n",f3(a,MAX));
+	
+	if(dlclose(handle)<0)
+	{
+		fprintf(stderr,"%s\n",dlerror());
+		exit(1);
+	}
+
+	return 0;
+}
---
+++ linuxprogram/week6/code/staticlib/lib.h
@@ -0,0 +1,7 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#define MAX 100
+int max(int*,int);
+int sum(int*,int);
+void urand(int *,int);
